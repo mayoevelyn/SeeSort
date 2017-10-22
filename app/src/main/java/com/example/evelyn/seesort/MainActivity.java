91f50512c5aff3c7d6b73a9188e524b554424d6c
Carlos <cpeza90@gmail.com>
2017-10-22 03:40:31 -0700

    Calling the Microsoft Cognitive Services API. Notes: This is just the sample code, modified to work on Android. A few notes:
    Permissions must be set in the Manifest to allow Internet Access.
    Network calls, if done on the Main thread, throw an exception and fail.
    The apache libraries are deprecated, but can still be used by adding the changes I made to the .gradle file.
    
    (This code is in no way representative of the way I'd recommend people to write their production code)

--- app/src/main/java/com/example/evelyn/seesort/MainActivity.java
+++ app/src/main/java/com/example/evelyn/seesort/MainActivity.java
@@ -9,7 +9,91 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.os.AsyncTask;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final String subscriptionKey = "00ca1b7a85b64189ad1bc3abd7b19eea";
+
+    // Replace or verify the region.
+    //
+    // You must use the same region in your REST API call as you used to obtain your subscription keys.
+    // For example, if you obtained your subscription keys from the westus region, replace
+    // "westcentralus" in the URI below with "westus".
+    //
+    // NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
+    // a free trial subscription key, you should not need to change this region.
+    public static final String uriBase = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze";
+
+
+    class RetrieveFeedTask extends AsyncTask<String, Void, String> {
+
+        private Exception exception;
+
+        protected String doInBackground(String... params) {
+            //Don't do this (massive Try/catch)
+            try {
+                HttpClient httpclient = new DefaultHttpClient();
+                URIBuilder builder = new URIBuilder(uriBase);
+
+                // Request parameters. All of them are optional.
+                builder.setParameter("visualFeatures", "Categories,Description,Color");
+                builder.setParameter("language", "en");
+
+                // Prepare the URI for the REST API call.
+                URI uri = builder.build();
+                HttpPost request = new HttpPost(uri);
+
+                // Request headers.
+                request.setHeader("Content-Type", "application/json");
+                request.setHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+                // Request body.
+                StringEntity reqEntity = new StringEntity("{\"url\":\"https://ih1.redbubble.net/image.92345144.8711/flat,800x800,075,t.u1.jpg\"}");
+                request.setEntity(reqEntity);
+
+                // Execute the REST API call and get the response entity.
+                HttpResponse response = httpclient.execute(request);
+                HttpEntity entity = response.getEntity();
+
+                if (entity != null)
+                {
+                    // Format and display the JSON response.
+                    String jsonString = EntityUtils.toString(entity);
+                    JSONObject json = new JSONObject(jsonString);
+                    System.out.println("REST Response:\n");
+                    System.out.println(json.toString(2));
+                }
+                return null;
+
+            }
+            catch(URISyntaxException ex) {
+
+            }
+            catch (IOException ex) {
+
+            }
+            catch(JSONException ex)
+            {
+
+            }
+            return null;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +102,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+        new RetrieveFeedTask().execute();
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
